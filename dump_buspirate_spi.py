e8bbb0882428da328758f1bfb5e45740f25ec821
Brian Hong <brian@hongs.me>
2018-07-27 15:51:02 -0400

    Added program arguments to dump_buspirate_spi.py

--- dump_buspirate_spi.py
+++ dump_buspirate_spi.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-import serial,time,struct,argparse
+import serial
+import time
+import struct
+import argparse
+import sys
 
 '''
     dump_buspirate_spi.py
@@ -16,17 +20,14 @@ import serial,time,struct,argparse
     http://dangerousprototypes.com/blog/2009/10/08/bus-pirate-raw-spi-mode/
 '''
 
-def s_open():
-    port = '/dev/ttyUSB0'
-    baud = 115200
-
+def s_open(port, baud):
     print('Opening serial port {}'.format(port))
     return serial.Serial(port, baud, timeout=0.001)
 
-def main():
+def main(port, baud, output):
 
     # Initialize serial port
-    ser = s_open()
+    ser = s_open(port, baud)
     ser.flushInput()
     ser.flushOutput()
 
@@ -47,12 +48,14 @@ def main():
 
     size = 32*1024*1024
     each_read = 4*1024
-    fn = 's0mething2.bin'
 
     # Dump
-    dump(ser, 0x0, size, each_read, fn)
+    dump(ser, 0x0, size, each_read, output)
 
 def dump(ser, beg, end, step, fn):
+
+    # TODO beginning address currently not used; always starts from zero
+    # This is because I'm too lazy to read on whether LSB goes first or last
 
     # Send a read command from 0x0
     send(ser, '[3 0 0 0')
@@ -97,4 +100,23 @@ def send(ser, msg):
     return data.rstrip()
 
 if __name__ == '__main__':
-    main()
+
+    parser = argparse.ArgumentParser(description='Buspirate SPI dumper', prog=sys.argv[0])
+
+    parser.add_argument(
+        '--port','-p',
+        help='Serial port device',
+        default='/dev/ttyUSB0')
+
+    parser.add_argument(
+        '--baud','-b',
+        help='Serial port baudrate',
+        default=115200)
+
+    parser.add_argument(
+        '--out','-o',
+        help='Dump output filename',
+        default='dump.bin')
+
+    args = parser.parse_args()
+    main(args.port, args.baud, args.out)
